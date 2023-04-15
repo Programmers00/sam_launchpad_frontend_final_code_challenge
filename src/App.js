d21c07abc0b4df46e7c22e6648bcd32211f70024
eunshinNoh <nes0410@gmail.com>
2023-04-15 15:16:20 -0600

    refs #3 install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 function App() {
-  return <div>sam_launchpad_frontend_final_code_challenge</div>;
+  return (
+    <div className="text-3xl font-bold underline">
+      sam_launchpad_frontend_final_code_challenge
+    </div>
+  );
 }
 export default App;
