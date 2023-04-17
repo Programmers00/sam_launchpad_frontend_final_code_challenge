2f171b4c1e43e39d12627d231a01b1f48acfbd85
eunshinNoh <nes0410@gmail.com>
2023-04-17 13:21:53 -0600

    refs #12 fetchPhotos using reset button in Header

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -43,7 +43,7 @@ const Header = () => {
             onClick={() =>
               page === "albums"
                 ? dispatch(setIsShowModalTrue())
-                : console.log("#reset")
+                : dispatch(fetchPhotos())
             }
             class={`${
               page === "albums"
